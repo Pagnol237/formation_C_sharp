37553457e0367ce87d11485cb8f7bdd92f83efd3
Hey there! I'm franck pagnol Nounjo <99295070+Pagnol237@users.noreply.github.com>
2025-09-09 00:00:17 -0500

    exo
    exercice 1

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace formation_C_sharp
     {
         static void Main(string[] arrgs)
         {
-            /* exercice 1 : Demander à l’utilisateur son prénom et son âge, puis afficher :*/
+            /* exercice 1 : Demander à l’utilisateur son prénom et son âge, puis afficher :
 
             Console.WriteLine("bonjour quel est votre nom ?");
             string nom = Console.ReadLine();
@@ -13,14 +13,31 @@ namespace formation_C_sharp
             int age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Bonjour " + nom + " tu as " + age + " ans.");
 
-            /* exercice 2: Demander à l’utilisateur de saisir la largeur et la hauteur d’un rectangle, puis afficher sa surface.
-            (surface = largeur × hauteur)*/
+             exercice 2: Demander à l’utilisateur de saisir la largeur et la hauteur d’un rectangle, puis afficher sa surface.
+            (surface = largeur × hauteur)
 
             Console.WriteLine("saisir la longeur de votre figure");
             int longeur = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("saisir la largeur de votre figure :");
             int largeur = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("solution (calcule de la surface)\n************************\n\n S = L*l\n A.N:" + longeur + "+" + largeur + "=" + longeur * largeur);
+            */
+            /* exerice 1 calcule de la moyenne*/
+            Console.WriteLine("saisir votre note de maths .../20");
+            double note_maths = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("saisir votre note de d\'informatique .../20");
+            double note_infos = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("saisir votre note  d\'édu .../20");
+            double note_edu = Convert.ToDouble(Console.ReadLine());
+
+            double moyenne = (note_edu + note_infos + note_maths) / 3;
+
+            string mention =(moyenne<10)?"ÉCHEC" : (moyenne > 10  && moyenne<12)? "PASSABLE": (moyenne >12 && moyenne<14)?" ASSEZ BIEN": (moyenne >14 && moyenne<16)?"BIEN":"TRÈS BIEN";
+
+            Console.WriteLine("Résultat \n----------------------\n");
+            Console.WriteLine($"maths :\t{note_maths}\ninfos :\t{note_infos}\nédu :\t{note_edu}\n*Moyenne :*\t{moyenne}\nMENTION : \t{mention}\n**********");
+
+
     }
   }
 }
--- obj/Debug/net9.0/formation_C_sharp.AssemblyInfo.cs
+++ obj/Debug/net9.0/formation_C_sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("formation_C_sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af18bbf7a87e656061645a2cd396d30c5f414f18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc54c623c8bedc66b4d41f056c7cb7511a191e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("formation_C_sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("formation_C_sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
