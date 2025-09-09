cdc54c623c8bedc66b4d41f056c7cb7511a191e9
Hey there! I'm franck pagnol Nounjo <99295070+Pagnol237@users.noreply.github.com>
2025-09-08 23:06:56 -0500

    saisir au clavier
    notion apprise
    1 comment afficher dans la console
    2 recuperer les éelement saisie au clavier
    3 convertion explicite du type de donnée ou cast

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace formation_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] arrgs)
+        {
+            /* exercice 1 : Demander à l’utilisateur son prénom et son âge, puis afficher :*/
+
+            Console.WriteLine("bonjour quel est votre nom ?");
+            string nom = Console.ReadLine();
+            Console.WriteLine("quel est votre age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bonjour " + nom + " tu as " + age + " ans.");
+
+            /* exercice 2: Demander à l’utilisateur de saisir la largeur et la hauteur d’un rectangle, puis afficher sa surface.
+            (surface = largeur × hauteur)*/
+
+            Console.WriteLine("saisir la longeur de votre figure");
+            int longeur = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("saisir la largeur de votre figure :");
+            int largeur = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("solution (calcule de la surface)\n************************\n\n S = L*l\n A.N:" + longeur + "+" + largeur + "=" + longeur * largeur);
+    }
+  }
+}
